7cbf18eb5576132671bad376b399fe7dc8195dc9
beingAaksh <74947468+beingAaksh@users.noreply.github.com>
2021-01-03 14:57:24 +0530

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -5,40 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public static Player PlayerObject;
    private const float PLAYER_SPEED = 20;
-
-    private const float SPELL_MAX_COOLDOWN = 0.2f;
-    private float SpellCoolDown;
     private bool Alive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerObject = this;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Alive)
-        {
-
-            if (SpellCoolDown > 0) SpellCoolDown -= Time.deltaTime;
-
-            if (SpellCoolDown <= 0)
-            {
-                if (Input.GetKey(KeyCode.Mouse0))
-                {
-                    SpellCoolDown = SPELL_MAX_COOLDOWN;
-                    GameObject projectile = Instantiate(Resources.Load("Spell")) as GameObject;
-
-                    projectile.transform.position = transform.position;
-                    projectile.GetComponent<Projectile>().Speed = 100;
-                    projectile.GetComponent<Projectile>().Angle = Mathf.Atan2(Input.mousePosition.y - Screen.height / 2f, Input.mousePosition.x - Screen.width / 2f);
-                }
-            }
-        }
+    
     }
 
     private void FixedUpdate()
